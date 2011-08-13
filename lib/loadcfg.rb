5a5edd870fead3600f7e96ef86baea0c8c730056
Jorge Rodriguez <jorge.rodriguez.suarez@gmail.com>
2011-08-12 20:30:40 -0700

    Add :enviroment and :ignore_not_found to options hash

--- lib/loadcfg.rb
+++ lib/loadcfg.rb
@@ -7,10 +7,15 @@ module LoadCfg
   end
     
   module ClassMethods
-    def loadconfig(yaml_file_name)
-      loaded_yaml = YAML.load_file(Rails.root.join("config/#{yaml_file_name}.yml"))[Rails.env]
-      loaded_yaml.keys.each do |key|
-        self.const_set("#{[yaml_file_name, '_', key].join.upcase}", loaded_yaml[key]) 
+    def loadconfig(yaml_file_name, options = {})
+      environment = (options[:environment] || Rails.env).to_s
+      loaded_yaml = YAML.load_file(Rails.root.join("config/#{yaml_file_name}.yml"))[environment]
+      if loaded_yaml.present?
+        loaded_yaml.keys.each do |key|
+          self.const_set("#{[yaml_file_name, '_', key].join.upcase}", loaded_yaml[key]) 
+        end
+      else
+        raise "Data not found on enviroment: '#{environment}'" unless options[:ignore_not_found]
       end
     end
   end
